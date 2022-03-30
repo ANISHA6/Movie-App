1454c34601fbb0cc91bf1570817af5672bbbc665
ANISHA6 <49230189+ANISHA6@users.noreply.github.com>
2022-03-30 15:24:32 +0530

    Subscribe to the store

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,47 @@
 import React from 'react';
+import {data} from '../data';
+import Navbar from './Navbar';
+import MovieCard from './MovieCard';
 
 
-function App() {
+class App extends React.Component{
+  componentDidMount(){
+    const {store}=this.props;
+    store.subscribe(()=>{
+      console.log('UPDATED',);//2
+      this.forceUpdate();
+    })
+    //make api call
+    //dispatch action i.e add movie to the store
+    store.dispatch({//1
+      type: 'ADD_MOVIES',
+      movies: data
+
+    });
+    console.log('STATE', this.props.store.getState());//3
+  }
+  render(){
+  const movies=this.props.store.getState();
+  console.log('RENDER')
   return (
     <div className="App">
-      Project Setup
+   <Navbar />
+   <div className="main">
+     <div className="tabs">
+       <div className="tab">Movies</div>
+       <div className="tab">Favourites</div>
+     </div>
+
+     <div className="list">
+       {movies.map((movie, index) =>(
+         <MovieCard movie={movie} key={`movies-${index}`} />//index added for unique key
+       ))}
+
+     </div>
+   </div>
     </div>
   );
+ }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
+
 import './index.css';
 import App from './components/App';
+import movies from './reducers'
+
+const store=createStore(movies);
+console.log('store', store);
+// console.log(' BEFORE STATE', store.getState());//Internally call the reducer to get the initial state
+
+// store.dispatch({
+//   type:'ADD_MOVIES',
+//   movies:[{name: 'superman'}]
+// });
+// console.log(' AFTER STATE', store.getState());//Internally call the reducer to get the initial state
+
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App store={store}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
